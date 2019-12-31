095a4d083310dad5d2a3f047dfc7bab9a11d37bb
voldikss <dyzplus@gmail.com>
2019-12-31 22:35:08 +0800

    Update

--- content.js
+++ content.js
@@ -7,129 +7,143 @@
 
 // Todo: 提醒打卡，入口做在popup页面内，https://www.shanbay.com/team/members/#p1
 
-!function () {
-    // get members table
-    let members_table = document.getElementById("members");
-    let members_table_body = document.querySelector("#members tbody");
+!(function() {
+  // get members table
+  let members_table = document.getElementById('members')
+  let members_table_body = document.querySelector('#members tbody')
 
-    // create members table
-    let dispel_members_table = document.createElement("table");
-    dispel_members_table.setAttribute("id", "dispel_members");
-    dispel_members_table.setAttribute("class", "table table-bordered");
-    dispel_members_table.appendChild(document.querySelector("#members thead").cloneNode(true));
-    document.getElementById("dismiss_container").insertBefore(dispel_members_table, members_table);
+  // create members table
+  let dispel_members_table = document.createElement('table')
+  dispel_members_table.setAttribute('id', 'dispel_members')
+  dispel_members_table.setAttribute('class', 'table table-bordered')
+  dispel_members_table.appendChild(
+    document.querySelector('#members thead').cloneNode(true)
+  )
+  document
+    .getElementById('dismiss_container')
+    .insertBefore(dispel_members_table, members_table)
 
-    // create tbody
-    let dispel_members_table_body = document.createElement("tbody");
-    dispel_members_table.appendChild(dispel_members_table_body);
+  // create tbody
+  let dispel_members_table_body = document.createElement('tbody')
+  dispel_members_table.appendChild(dispel_members_table_body)
 
-    // table title
-    let dispel_members_table_title = document.createElement("caption");
-    dispel_members_table_title.innerText = "续命失败，-1s!!!";
-    dispel_members_table.insertBefore(dispel_members_table_title, document.querySelector("#dispel_members tbody")[0]);
+  // table title
+  let dispel_members_table_title = document.createElement('caption')
+  dispel_members_table_title.innerText = '续命失败，-1s!!!'
+  dispel_members_table.insertBefore(
+    dispel_members_table_title,
+    document.querySelector('#dispel_members tbody')[0]
+  )
 
-    // get the number of pages
-    let pagination = document.getElementsByClassName("pagination")[0];
-    let pages = pagination.getElementsByClassName("endless_page_link");
-    let page_number = parseInt(pages[pages.length - 2].innerHTML);
+  // get the number of pages
+  let pagination = document.getElementsByClassName('pagination')[0]
+  let pages = pagination.getElementsByClassName('endless_page_link')
+  let page_number = parseInt(pages[pages.length - 2].innerHTML)
 
-    // loading page
-    let loading = document.createElement("div");
-    loading.setAttribute("id", "loading_container");
-    loading.innerHTML = "<h1>正在续命，请稍后......</h1>";
-    document.body.appendChild(loading);
+  // loading page
+  let loading = document.createElement('div')
+  loading.setAttribute('id', 'loading_container')
+  loading.innerHTML = '<h1>正在续命，请稍后......</h1>'
+  document.body.appendChild(loading)
 
-    // create button: Check Me!
-    let check_btn = document.createElement("button");
-    check_btn.innerText = "一键查卡";
-    check_btn.setAttribute("id", "check_btn");
-    check_btn.addEventListener("click", CheckMe);
-    document.querySelector(".page-header").appendChild(check_btn);
+  // create button: Check Me!
+  let check_btn = document.createElement('button')
+  check_btn.innerText = '一键查卡'
+  check_btn.setAttribute('id', 'check_btn')
+  check_btn.addEventListener('click', CheckMe)
+  document.querySelector('.page-header').appendChild(check_btn)
 
-    // check and check, check every one!!!
-    function CheckMe() {
-        // Loading
-        let loading = document.getElementById("loading_container");
-        loading.style.display = "block";
+  // check and check, check every one!!!
+  function CheckMe() {
+    // Loading
+    let loading = document.getElementById('loading_container')
+    loading.style.display = 'block'
 
-        // display dispel members table
-        dispel_members_table.style.display = "table";
+    // display dispel members table
+    dispel_members_table.style.display = 'table'
 
-        // restore the expire contents
-        members_table_body.innerHTML = "";
-        dispel_members_table_body.innerHTML = "";
+    // restore the expire contents
+    members_table_body.innerHTML = ''
+    dispel_members_table_body.innerHTML = ''
 
-        // Post(page_number);
-        Post(page_number);
+    // Post(page_number);
+    Post(page_number)
+  }
+
+  /**
+   * @return {boolean}
+   */
+  // group living rule
+  function DispelQ(role, days, rate, checked1, checked2) {
+    if (role !== '2') {
+      return false
     }
-
-    /**
-     * @return {boolean}
-     */
-    // group living rule
-    function DispelQ(role, days, rate, checked1, checked2) {
-        if (role !== "2") {
-            return false;
-        }
-        // 三天以内断卡则踢
-        else if (parseInt(days) <= 3) {
-            return (checked2 === "未打卡")
-        }
-        // 七天以内两天不打卡则踢
-        else if (parseInt(days) <= 7 && parseFloat(rate) < 100) {
-            return (checked2 === "未打卡")
-        }
-        // 后期打卡率低于85%则踢
-        else if (parseFloat(rate) < 85.0) {
-            return true;
-        }
-        // 打卡率低于95%者连续两天不打卡则踢
-        else if (parseFloat(rate) < 95.0) {
-            return (checked1 === "未打卡" && checked2 === "未打卡")
-        }
-        return false;
+    // 三天以内断卡则踢
+    else if (parseInt(days) <= 3) {
+      return checked2 === '未打卡'
     }
+    // 七天以内两天不打卡则踢
+    else if (parseInt(days) <= 7 && parseFloat(rate) < 100) {
+      return checked2 === '未打卡'
+    }
+    // 后期打卡率低于85%则踢
+    else if (parseFloat(rate) < 85.0) {
+      return true
+    }
+    // 打卡率低于95%者连续两天不打卡则踢
+    else if (parseFloat(rate) < 95.0) {
+      return checked1 === '未打卡' && checked2 === '未打卡'
+    }
+    return false
+  }
 
-    /**
-     * @return {number}
-     */
-    // use recursion here
-    function Post(page_index) {
-        if (page_index === 0) {
-            // hide the loading page, maybe some improvements...
-            document.getElementById("loading_container").style.display = "none";
-            // remove pagination at the bottom
-            if (document.getElementsByClassName("pagination").length !== 0) {
-                pagination.parentNode.removeChild(pagination);
+  /**
+   * @return {number}
+   */
+  // use recursion here
+  function Post(page_index) {
+    if (page_index === 0) {
+      // hide the loading page, maybe some improvements...
+      document.getElementById('loading_container').style.display = 'none'
+      // remove pagination at the bottom
+      if (document.getElementsByClassName('pagination').length !== 0) {
+        pagination.parentNode.removeChild(pagination)
+      }
+      return 1
+    } else {
+      let req = new XMLHttpRequest()
+      let url = 'https://www.shanbay.com/team/manage/?page=' + page_index.toString()
+      req.open('GET', url, true)
+      req.responseType = 'document'
+      req.send()
+      req.onreadystatechange = function() {
+        if (req.status === 200 && req.readyState === 4) {
+          let members = req.response.querySelector('#members tbody').rows
+          for (let member of members) {
+            role = member.getAttribute('role')
+            days = member.children[2].innerText.trim()
+            rate = member.children[3].innerText.trim()
+            checked1 = member.children[4].innerText.trim()
+            checked2 = member.children[5].innerText.trim()
+            if (DispelQ(role, days, rate, checked1, checked2)) {
+              dispel_members_table_body.appendChild(member.cloneNode(true))
             }
-            return 1;
+            members_table_body.appendChild(member.cloneNode(true))
+          }
+          page_index--
+          // setTimeout()...?
+          Post(page_index)
         }
-        else {
-            let req = new XMLHttpRequest();
-            let url = "https://www.shanbay.com/team/manage/?page=" + page_index.toString();
-            req.open("GET", url, true);
-            req.responseType = "document";
-            req.send();
-            req.onreadystatechange = function () {
-                if (req.status === 200 && req.readyState === 4) {
-                    let members = req.response.querySelector("#members tbody").rows;
-                    for (let member of members) {
-                        role = member.getAttribute("role");
-                        days = member.children[2].innerText.trim();
-                        rate = member.children[3].innerText.trim();
-                        checked1 = member.children[4].innerText.trim();
-                        checked2 = member.children[5].innerText.trim();
-                        if (DispelQ(role, days, rate, checked1, checked2)) {
-                            dispel_members_table_body.appendChild(member.cloneNode(true));
-                        }
-                        members_table_body.appendChild(member.cloneNode(true));
-                    }
-                    page_index--;
-                    // setTimeout()...?
-                    Post(page_index);
-                }
-            }
-        }
+      }
     }
-}();
+  }
 
+  let text = 'test'
+  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    switch (message.type) {
+      case 'check':
+        sendResponse(text)
+        break
+    }
+  })
+})()
--- popup.js
+++ popup.js
@@ -5,13 +5,31 @@
  * @Last Modified time: 2018-10-20 08:49:44
  */
 
-!function () {
-    document.getElementById("manager").addEventListener("click", function () {
-        window.open("https://www.shanbay.com/team/manage", "_blank");
-    });
+!(function() {
+  document.getElementById('manager').addEventListener('click', () => {
+    window.open('https://www.shanbay.com/team/manage', '_blank')
+  })
 
-    document.getElementById("reminder").addEventListener("click", function () {
-        // alert("待开发?");
+  document.getElementById('check').addEventListener('click', () => {
+    chrome.tabs.query({}, tabs => {
+      tabs.forEach(tab => {
+        chrome.tabs.sendMessage(tab.id, { type: 'check' }, response => {
+          // alert(response)
+          if (response){
+            alert(response)
+          }
+        })
+      })
     })
-}();
+  })
 
+  document.getElementById('add').addEventListener('click', () => {
+    let text = document.getElementById('members').value
+    let members = JSON.parse(text)
+    chrome.storage.sync.set({ members: members }, () => {
+      chrome.storage.sync.get(null, members => {
+        console.log(members)
+      })
+    })
+  })
+})()
